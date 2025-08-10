e32540ba476d2de50fe1972568a074519451fbb7
Thiago Luiz <rp.thiagoluiz@gmail.com>
2025-08-10 19:22:14 -0300

    feat: create db connection

--- command.sh
+++ command.sh
@@ -1,4 +1,7 @@
-# Raiz - criar as tabelas
+#Criar o banco de dados
+npx drizzle-kit generate
+
+# Raiz - atualizar o banco de dados
 npx drizzle-kit push
 
 # Seeds - criar os dados iniciais
@@ -6,3 +9,6 @@ npx tsx --env-file=.env src/db/seed.ts
 
 # Studio
 npx drizzle-kit studio
+
+# BetterAuth
+npx @better-auth/cli@1.2.12 generate
